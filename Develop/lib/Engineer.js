f7c3375f0c4ee8f8bcb0495e95952b777859a696
Mengmei Tu <redmmeii@gmail.com>
2020-12-21 15:20:46 -0600

    update engineer.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -3,3 +3,22 @@
 // github  // GitHub username
 // getGithub()
 // getRole() // Overridden to return 'Engineer'
+
+const Employee = require("./Employee");
+
+module.exports = class Engineer extends Employee {
+    constructor(github, name, id, email) {
+        super(name, id, email);
+        this.github = github;
+    }
+
+    getGithub() {
+        return this.github;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+}
+
+
