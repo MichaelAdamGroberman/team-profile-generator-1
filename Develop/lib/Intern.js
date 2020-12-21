62fd69b98c4c4ef836997bddf5317026d1def524
Mengmei Tu <redmmeii@gmail.com>
2020-12-21 15:31:38 -0600

    finish intern.js and passed test

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -4,4 +4,19 @@
 // getSchool()
 // getRole() // Overridden to return 'Intern'
 
+const Employee = require("./Employee");
 
+module.exports = class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+
+    getSchool() {
+        return this.school;
+    }
+
+    getRole() {
+        return "Intern";
+    }
+}
