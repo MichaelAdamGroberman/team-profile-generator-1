d08819bf9da0bdaef3eeb4f197d8df805837b57c
Mengmei Tu <redmmeii@gmail.com>
2020-12-29 19:26:02 -0600

    add validate for questions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const emailValidator = require('email-validator');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -21,21 +22,35 @@ const questions = {
             type: "input",
             name: "name",
             message: "What is the manager's name?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter manager's name." }
+            },
         },
         {
             type: "input",
             name: "id",
             message: "What is the manager's id?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter manager's id." }
+            },
         },
         {
             type: "input",
             name: "email",
             message: "What is the manager's email address?",
+            validate: (value) => {
+                if (emailValidator.validate(value)) {
+                    return true
+                } else { return 'Please enter a valid email address.' }
+            },
         },
         {
             type: "input",
             name: "officeNumber",
             message: "What is the manager's office number?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter manager's office number." }
+            },
         },
         {
             type: "list",
@@ -50,21 +65,35 @@ const questions = {
             type: "input",
             name: "name",
             message: "What is the engineer's name?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter engineer's name." }
+            },
         },
         {
             type: "input",
             name: "id",
             message: "What is the engineer's id?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter engineer's id." }
+            },
         },
         {
             type: "input",
             name: "email",
             message: "What is the engineer's email address?",
+            validate: (value) => {
+                if (emailValidator.validate(value)) {
+                    return true
+                } else { return 'Please enter a valid email address.' }
+            },
         },
         {
             type: "input",
             name: "github",
             message: "What is the engineer's GitHub username?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter engineer's GitHub." }
+            },
         },
         {
             type: "list",
@@ -79,21 +108,35 @@ const questions = {
             type: "input",
             name: "name",
             message: "What is the intern's name?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter intern's name." }
+            },
         },
         {
             type: "input",
             name: "id",
             message: "What is the intern's id?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter intern's id." }
+            },
         },
         {
             type: "input",
             name: "email",
             message: "What is the intern's email address?",
+            validate: (value) => {
+                if (emailValidator.validate(value)) {
+                    return true
+                } else { return 'Please enter a valid email address.' }
+            },
         },
         {
             type: "input",
             name: "school",
             message: "What school is the intern attending?",
+            validate: (value) => {
+                if (value) { return true } else { return "Please enter the name of school." }
+            },
         },
         {
             type: "list",
